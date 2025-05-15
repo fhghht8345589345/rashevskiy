91a972209bdcac8369d7cc0048f160ac15318969
Aleksand golovanov <golovanovbiba@gmail.com>
2025-05-15 18:03:21 +0300

    xomak2

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 window.addEventListener("DOMContentLoaded",function () {
     let gg = document.getElementById("gg");
-
+let hp = 0;
+let ph = 0;
+let ENG = document.getElementById("ENG");
+let brus = document.getElementById("button");
+let lrus = document.getElementById("blerinacapuchino");
+if (localStorage.getItem("ENG"))
+{
+    hp = parseInt( localStorage.getItem("ENG"))
+    ENG.innerHTML = hp
+} 
+if (localStorage.getItem("MLRS"))
+{
+    ph = parseInt( localStorage.getItem("MLRS"))
+    lrus.innerHTML = ph
+} 
     gg.addEventListener("click",function (){
-        alert("lorem20")
+    hp += 1+ph
+    localStorage.setItem("ENG",hp)
+        ENG.innerHTML = hp+ph
+
+    })
+    brus.addEventListener("click",function (){
+    ph += 1
+    hp-=1000
+    localStorage.setItem("MLRS",ph)
+    localStorage.setItem("ENG",hp)
+        lrus.innerHTML = ph
+        ENG.innerHTML = hp
+
     })
 })
+
